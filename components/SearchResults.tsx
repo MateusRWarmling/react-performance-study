2565a249514b8e8d50130429aeb79d4a6d74b1c4
Mateus Warmling <mateuswarmling@gmail.com>
2021-09-27 13:54:08 -0300

    first commit

---
+++ components/SearchResults.tsx
@@ -0,0 +1,29 @@
+import { ProductItem } from "./ProductItem";
+import { useMemo } from 'react'
+
+interface SearchResultsProps {
+    results: Array<{
+        id: number;
+        price: number;
+        title: string;
+    }>
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+    const totalPrice = useMemo(() => {
+        return results.reduce((total, product) => {
+            return total + product.price;
+        }, 0)
+    }, [results])
+    
+    return (
+        <div>
+            <h2>{totalPrice}</h2>
+            {results.map(product => {
+                return (
+                    <ProductItem product={product} />
+                );
+            })}
+        </div>
+    );
+}
